bda0bfe36a092e32f075f86236648e796432b99a
Saku Pöyri <saku.poyri@nordcloud.com>
2025-12-12 15:04:27 +0200

    first commit

---
+++ Day01/main.py
@@ -0,0 +1,22 @@
+file = open("input.txt")
+
+list = file.read().split("\n")[:-1]
+list = [(inst[0], int(inst[1:])) for inst in list]
+
+point = 50
+count = 0
+
+for i in list:
+    move = i[1]
+    count += move // 100
+    move = move % 100
+    if i[0] == "L":
+        count -= (point == 0)
+        point = (point - move)
+    else:
+        point = (point + move)
+    if move > 0:
+        count += (point <= 0 or point >= 100)
+    point = point % 100
+
+print(count)
---
+++ Day02/main.py
@@ -0,0 +1,18 @@
+file = open("input.txt")
+
+list = file.read().split("\n")
+list = list[0].split(",")
+list = [nums.split("-") for nums in list]
+
+sum = 0
+
+for nums in list:
+    start = int(nums[0])
+    end = int(nums[1])
+    for i in range(start, end+1):
+        id = str(i)
+        length = len(id)
+        if length % 2 == 0 and id[:(length//2)] == id[(length//2):]:
+            sum += i
+
+print(sum)
