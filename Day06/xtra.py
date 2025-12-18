3134657d504c661ab7d5eab9864e61987b67ce44
spoyri <saku.poyri@gmail.com>
2025-12-18 10:53:46 +0200

    Completed day 6 bonus

--- Day06/xtra.py
+++ Day06/xtra.py
@@ -1,13 +1,32 @@
 file = open("Day06/input.txt")
 lines = file.read().splitlines()
-lines = [line.split(" ") for line in lines]
-lines = [list(filter(lambda x: x != '', line)) for line in lines]
+
+equations = []
+i = 0
+equation = []
+while i < len(lines[0]):
+    if lines[4][i] != " ":
+        equation = []
+        equations.append(equation)
+        equation.append(lines[4][i])
+        num = lines[0][i] + lines[1][i] + lines[2][i] + lines[3][i]
+        equation.append(int(num))
+    else:
+        num = lines[0][i] + lines[1][i] + lines[2][i] + lines[3][i]
+        if num != "    ":
+            equation.append(int(num))
+    i += 1
 
 sum = 0
-for i in range(len(lines[0])):
-    if lines[4][i] == "*":
-        sum += int(lines[0][i]) * int(lines[1][i]) * int(lines[2][i]) * int(lines[3][i])
+for equation in equations:
+    if equation[0] == "*":
+        result = 1
+        for j in range(1, len(equation)):
+            result *= equation[j]
     else:
-        sum += int(lines[0][i]) + int(lines[1][i]) + int(lines[2][i]) + int(lines[3][i])
+        result = 0
+        for j in range(1, len(equation)):
+            result += equation[j]
+    sum += result
 
 print(sum)
