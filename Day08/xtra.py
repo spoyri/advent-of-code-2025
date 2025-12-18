a453f30b75da457e40cfe3463e1ec7ad40a0490d
spoyri <saku.poyri@gmail.com>
2025-12-18 16:33:39 +0200

    Completed day 8

---
+++ Day08/xtra.py
@@ -0,0 +1,41 @@
+import math
+
+file = open("Day08/input.txt")
+lines = file.read().splitlines()
+lines = [[int(num) for num in line.split(",")] for line in lines]
+
+distances = []
+
+groups = [[0]]
+lines[0].append(0)
+
+for i in range(1, len(lines)):
+    groups.append([i])
+    lines[i].append(i)
+    cord1 = lines[i]
+    for j in range(i):
+        cord2 = lines[j]
+        distance = math.sqrt(math.pow(cord1[0] - cord2[0], 2) + math.pow(cord1[1] - cord2[1], 2) + math.pow(cord1[2] - cord2[2], 2))
+        distances.append([i, j, distance])
+
+distances.sort(key=lambda x: x[2])
+
+i = 0
+grouplen = 0
+while grouplen != 1000:
+    distance = distances[i]
+    cord1 = lines[distance[0]]
+    cord2 = lines[distance[1]]
+    print(f"Connecting indexes {distance[0]} and {distance[1]}")
+    if cord1[3] != cord2[3]:
+        newGroup = groups[cord1[3]].extend(groups[cord2[3]])
+        groups[cord2[3]] = []
+        groupIndex = cord1[3]
+        for index in groups[cord1[3]]:
+            lines[index][3] = groupIndex
+        grouplen = len(groups[cord1[3]])
+        if grouplen == 1000:
+            print(cord1[0] * cord2[0])
+    else:
+        print("Already connected!")
+    i += 1
